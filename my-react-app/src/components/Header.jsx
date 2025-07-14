08596a6c082cda2ece2dd86451ff40e81017abb2
tbot53 <dhavihd@gmail.com>
2025-07-14 19:06:02 +0100

    changed the name in the header section and title section

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(props) {
     return(
         <header className="flex space-x-4 border-b-1 border-gray-500 items-center justify-between p-4">
-            <h1 className="text-3xl font-bold">QuizTime</h1>
+            <h1 className="text-3xl font-bold">ACETHIS QUIZ</h1>
             <button className="h-fit w-fit rounded-lg p-4 flex items-center bg-blue-500 hover:bg-blue-600 text-white text-2xl cursor-pointer" onClick={props.createQuiz}>
                 Start a new quiz
             </button>
